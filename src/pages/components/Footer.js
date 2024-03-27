ae495e6e4725e8adc23e5d3d1527f328c5237c7a
wssfernando99 <sarindusauranga@gmail.com>
2024-03-27 11:50:04 +0530

    Create Footer

---
+++ src/pages/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './footer.css'
+
+export const Footer = () => {
+  return (
+    <div className='footer-main-container'>
+        <h1>This is Footer</h1>
+    </div>
+  )
+}
