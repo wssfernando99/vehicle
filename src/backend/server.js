a5bb11a1f5fd8cf2c35be88b5544dcc084747452
wssfernando99 <sarindusauranga@gmail.com>
2024-06-10 03:19:48 +0530

    create the connection with mongodb

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,55 +1,30 @@
 const express = require("express");
-const mysql = require('mysql');
+const mongoose = require('mongoose')
+const bodyParser= require('body-parser')
 const cors = require('cors');
+require('donenv').configs();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "signup"
-})
+app.use(bodyParser.json())      //to send json format data
+app.use(cors());        //This control is essential for security.
+app.use(express.json());           
 
-app.post('/signup', (req, res) =>{
-    const sql = "INSERT INTO login (`name`,`email`,`password`) VALUES (?)";
-    const values = [
-        req.body.name,
-        req.body.email,
-        req.body.password
-    ]
-    db.query(sql, [values], (err, data) =>{
-        if(err) {
-            return res.json("Error");
-        }
-        return res.json(data);
-    })
-    
-})
-app.post('/login', (req, res) =>{
-    const sql = "SELECT * FROM login WHERE `email` =? AND `password`=? ";
-    const values = [
-        req.body.email,
-        req.body.password
-    ]
-    db.query(sql, [req.body.email,req.body.password], (err, data) =>{
-        if(err) {
-            return res.json("Error");
-        }
-        else if(data.length > 0){
-            return res.json("Success");
-        }
-        else{
-            return res.json("Fail");
-        }
-    })
-    
-      
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL);
+
+const connection = mongoose.connection
+
+connection.once('open',()=>{
+    console.log('MonoDB Connected successfully'); //connect to the databse if there is no error
 
 })
+connection.on('error',()=>{
+    console.log(`error with connecting MongoDB`); //if there is an error console.log  it
+})
 
-app.listen(8082 , ()=>{
+const port = process.env.PORT || 8082
+app.listen(port ,()=>{
     console.log("listening")
 })
