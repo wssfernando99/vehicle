e9d293db8c471f6825d704fe3602eb7fa1aa2fc3
wssfernando99 <sarindusauranga@gmail.com>
2024-06-12 01:39:39 +0530

    Correction

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose')
 const bodyParser= require('body-parser')
 const cors = require('cors');
-require('donenv').configs();
+require('dotenv').config();
 const userRouter = require('./routes/User')
 
 const app = express();
