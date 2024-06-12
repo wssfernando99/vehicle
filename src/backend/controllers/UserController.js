fb76363176bfd80a4bd5d3834ecfab5bca122cd9
wssfernando99 <sarindusauranga@gmail.com>
2024-06-12 23:17:29 +0530

    create login function

--- src/backend/controllers/UserController.js
+++ src/backend/controllers/UserController.js
@@ -5,12 +5,12 @@ exports.register = (req,res)=>{
     const email = req.body.email;
     const password = req.body.password;
 
-    const newUser = new User({
+    const user = new User({
         name,
         email,
         password
     });
-    newUser.save().then(()=>{
+    user.save().then(()=>{
         res.json("Uer details added successfully")
     }).catch((err)=>{
         console.log(err);
@@ -18,5 +18,19 @@ exports.register = (req,res)=>{
 }
 
 exports.login = (req, res) => {
-    
+    User.findOne({email:req.body.email}).then((user)=>{
+        if(!user){
+            return res.status(404).json({success:false,message:"user email not found"});
+        }else{
+            user.comparePassword(req.body.password, (err,isMatch)=>{
+                if(!isMatch){
+                    return res.status(404).json({success:fail,message:"Your passowrd is wrong"})
+                }else{
+                    res.status(200).json({success:true,message:"Successfully Logged in!"})
+                }
+            })
+        }
+    }).catch((err)=>{
+        console.log(err);
+    })
 };
