3c850a8f7505b25bbc72a308374f3ae2bf53deb4
wssfernando99 <sarindusauranga@gmail.com>
2024-06-12 23:17:10 +0530

    Update user model
    add password comparison function and hash the password.

--- src/backend/models/User.js
+++ src/backend/models/User.js
@@ -1,4 +1,5 @@
-
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -17,7 +18,32 @@ const userSchema = new Schema({
         required:true
     }
 })
-
+// Saving user data
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
+        //checking if password field is available and modified
+        bcrypt.genSalt(SALT, function (err, salt) {
+            if (err) return next(err)
+        
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
+     
+// For comparing the users entered password with database duing login 
+UserSchema.methods.comparePassword = function (candidatePassword, callBack) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return callBack(err);
+        callBack(null, isMatch);
+    });
+};
 
 const User = mongoose.model('User',userSchema);
 module.exports = User;
